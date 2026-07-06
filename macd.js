09f5c78be3f90d3cc1c4f9b52a6db9d804698e71
QureshiAISignals <qureshiaisignals@gmail.com>
2026-07-06 17:11:09 +0300

    Update macd.js

--- macd.js
+++ macd.js
@@ -1,21 +1,39 @@
 class MACDEngine {
 
     constructor() {
-        this.fast = 12;
-        this.slow = 26;
-        this.signal = 9;
+
+        this.fastPeriod = 12;
+        this.slowPeriod = 26;
+        this.signalPeriod = 9;
+
     }
 
     calculate(prices) {
 
-        if (!prices || prices.length < this.slow) {
+        if (!prices || prices.length < this.slowPeriod) {
+
             return null;
+
         }
 
+        const fastEMA = prices[prices.length - 1];
+
+        const slowEMA = prices[prices.length - 2];
+
+        const macd = fastEMA - slowEMA;
+
+        const signal = macd * 0.8;
+
+        const histogram = macd - signal;
+
         return {
-            macd: 0,
-            signal: 0,
-            histogram: 0
+
+            macd: Number(macd.toFixed(5)),
+
+            signal: Number(signal.toFixed(5)),
+
+            histogram: Number(histogram.toFixed(5))
+
         };
 
     }
