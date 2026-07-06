bfb09f6a809cca4e103ca30f9263dc54f5739f49
QureshiAISignals <qureshiaisignals@gmail.com>
2026-07-06 17:14:20 +0300

    Update cci.js

--- cci.js
+++ cci.js
@@ -6,12 +6,36 @@ class CCIEngine {
 
     calculate(highs, lows, closes) {
 
-        if (!highs || !lows || !closes) {
+        if (
+            !highs || !lows || !closes ||
+            highs.length < this.period ||
+            lows.length < this.period ||
+            closes.length < this.period
+        ) {
             return null;
         }
 
+        const typicalPrices = [];
+
+        for (let i = closes.length - this.period; i < closes.length; i++) {
+            typicalPrices.push((highs[i] + lows[i] + closes[i]) / 3);
+        }
+
+        const sma =
+            typicalPrices.reduce((a, b) => a + b, 0) / this.period;
+
+        const meanDeviation =
+            typicalPrices.reduce((a, b) => a + Math.abs(b - sma), 0) / this.period;
+
+        if (meanDeviation === 0) {
+            return { cci: 0 };
+        }
+
+        const currentTP = typicalPrices[typicalPrices.length - 1];
+        const cci = (currentTP - sma) / (0.015 * meanDeviation);
+
         return {
-            cci: 0
+            cci: Number(cci.toFixed(2))
         };
 
     }
