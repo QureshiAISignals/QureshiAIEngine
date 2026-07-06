6df117d460fab41f40124986be6d20ce37803cc2
QureshiAISignals <qureshiaisignals@gmail.com>
2026-07-06 05:00:27 +0300

    Create candlestick.js

---
+++ candlestick.js
@@ -0,0 +1,33 @@
+class CandlestickEngine {
+
+    bullishEngulfing(previous, current) {
+
+        return false;
+
+    }
+
+    bearishEngulfing(previous, current) {
+
+        return false;
+
+    }
+
+    doji(candle) {
+
+        return false;
+
+    }
+
+    hammer(candle) {
+
+        return false;
+
+    }
+
+    shootingStar(candle) {
+
+        return false;
+
+    }
+
+}
